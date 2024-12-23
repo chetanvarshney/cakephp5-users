c7d06ba7ec3087f5f1469535c26acbd69cbe43ba
Chetan Varshney <chetanvarshney@gmail.com>
2024-12-23 22:52:39 +0530

    Plugin version 5.0.1

---
+++ templates/UserGroupPermissions/groups.php
@@ -0,0 +1,3 @@
+Source code is available on https://www.codinghotspot.com/item/cakephp-5.x-user-management-plugin-with-twitter-bootstrap-5.x/15
+
+Demo on https://cakephp5-user-management.ektanjali.com/
